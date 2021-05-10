7bffafe468afadd431f9f58cba9a2c80ea87c97c
EC2 Default User <ec2-user@ip-172-31-27-213.us-east-2.compute.internal>
2021-05-10 16:06:38 +0000

    first commit

---
+++ js/main4.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".slick").slick({
+        autoplay:true,
+        autoplaySpeed: 4000,
+        dots: true,
+        arrows: true,
+        prevArrow:'<div class="prev">＜</div>',
+        nextArrow:'<div class="next">＞</div>',
+        dotsClass: "slide-dots",
+    });
+});
